19601a7990adfe41a137d4b81dcb09146359c961
MarcinRG <marcin.guszpit@gmail.com>
2023-01-02 13:38:01 +0100

    init

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 
-// app.set("view engine","ejs");
-// app.set("views",path.join(__dirname, "templates"));
-// app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname, 'templates'));
+app.use(express.static(path.join(__dirname,'static')));
 
 app.all('*',(req,res,next)=>{
     console.log(req.path);
     console.log(req.query)
-    console.log(path.join(__dirname,'public').toString());
     next();
 })
-
-
 
 app.get('/',(req,res)=>{
     res.send('To jest tekst');
