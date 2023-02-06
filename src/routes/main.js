1a29f246cd1e6a1bbc9107c8184f77ddc3b9f7ab
MarcinRG <marcin.guszpit@gmail.com>
2023-02-06 14:59:32 +0100

    changes

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const {check, validationResult} = require('express-validator/check');
+
+
 const router = express.Router();
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: 'PASTE_REAL_KEY_HERE'
+    }
+}));
+
 
 router.get('/', (req, res, next) => {
     res.render('./main-page/index.ejs');
 });
 
 router.post('/', (req, res, next) => {
+    transporter.sendMail({
+        to: 'biuro@pphu-adampol.pl',
+        from: 'no-reply-form@pphu-adampol.pl',
+        subject: 'Wiadomość ze strony WWW',
+        html: '<span>To jest wiadomość ze strony</span>'
+
+    }).then(() => {
+        console.log('wysłanie w porządku');
+    })
+
+
     res.render('error-custom-msg.ejs', {
         error: null,
         title: 'Dziękujemy za wiadomość',
